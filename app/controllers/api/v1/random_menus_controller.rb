586dac13c697d886ae5b6f007668c09cb5a1aece
aiandrox <aiandrox7@gmail.com>
2025-05-24 11:23:33 +0900

    N+1の解消

--- app/controllers/api/v1/random_menus_controller.rb
+++ app/controllers/api/v1/random_menus_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class RandomMenusController < BaseController
       def index
-        menus = Menu.order("RAND()").limit(params[:limit] || 10)
+        menus = Menu.includes(:genre, :noodle, :soup).order("RAND()").limit(params[:limit] || 10)
         menu_list = ApiEntity::MenuList.new(menus:)
         render json: menu_list, status: :ok
       end
