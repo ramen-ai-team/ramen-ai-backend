8928a272040f43dd003adb429cfbdb93c97b0f3a
aiandrox <aiandrox7@gmail.com>
2025-05-24 10:59:49 +0900

    limitにする

--- app/controllers/api/v1/random_menus_controller.rb
+++ app/controllers/api/v1/random_menus_controller.rb
@@ -2,9 +2,8 @@ module Api
   module V1
     class RandomMenusController < BaseController
       def index
-        pagy, menus = pagy(Menu.order("RAND()"))
+        menus = Menu.order("RAND()").limit(params[:limit] || 10)
         menu_list = ApiEntity::MenuList.new(menus:)
-        pagy_headers_merge(pagy)
         render json: menu_list, status: :ok
       end
     end
