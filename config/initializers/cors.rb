6cfd4aa47f3e288764b5d78df5a045c95e3a92e3
高橋　知咲 <takahashichisaki@wendyMacBook-Air.local>
2025-06-30 17:34:03 +0900

    crosを設定変更

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,9 +7,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
+<<<<<<< HEAD
     allowed_origins = case Rails.env
       when "production"
-        ["https://ramen-ai-frontend.vercel.app", "https://ramen-ai-admin.vercel.app", "http://localhost:8081"]
+        ["https://ramen-ai-frontend.vercel.app", "https://ramen-ai-admin.vercel.app", "http://localhost:8081", "http://localhost:3000", "https://ramen-ni-ai-wo.vercel.app/"]
       when "development"
         ["http://localhost:8081", "http://localhost:3001"]
       else
