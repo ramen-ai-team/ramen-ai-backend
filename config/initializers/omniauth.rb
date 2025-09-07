329df0cca9d4cfa30eb5cf1bdc323aac38486215
aiandrox <aiandrox7@gmail.com>
2025-09-07 17:28:01 +0900

    テスト環境用の変更

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 # Google OAuth credentials need to be configured
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, Rails.application.credentials.gcp[:oauth_client_id], Rails.application.credentials.gcp[:oauth_client_secret], {
+  provider :google_oauth2, Rails.application.credentials.dig(:gcp, :oauth_client_id), Rails.application.credentials.dig(:gcp, :oauth_client_secret), {
     scope: "openid,profile,email",
     prompt: "select_account",
     image_aspect_ratio: "square",
