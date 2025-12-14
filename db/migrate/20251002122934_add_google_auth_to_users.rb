921b5c6149352e36ca09fbd285878821ffb66fcd
aiandrox <aiandrox7@gmail.com>
2025-12-14 11:53:35 +0900

    migrationの修正

--- db/migrate/20251002122934_add_google_auth_to_users.rb
+++ db/migrate/20251002122934_add_google_auth_to_users.rb
@@ -1,7 +1,6 @@
 class AddGoogleAuthToUsers < ActiveRecord::Migration[8.0]
   def change
     add_column :users, :google_id, :string
-    add_column :users, :image, :string
     add_column :users, :email_verified, :boolean, default: false
 
     add_index :users, :google_id, unique: true
